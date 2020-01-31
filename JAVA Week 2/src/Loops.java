983540f90156f60ee73115802fa975e5d4e2a021
Desmond Young <60267316+DesmondYo@users.noreply.github.com>
2020-01-31 11:43:26 -0700

    Update Loops.java

--- JAVA Week 2/src/Loops.java
+++ JAVA Week 2/src/Loops.java
@@ -4,23 +4,23 @@ public class Loops {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-//		int i=0;
-//		int num=0;
-//		while (i<51)
-//		{
+		int i=0;
+		int num=0;
+		while (i<51)
+		{
 		
-//			System.out.println(num);
-//			i+=1;
-//			num+=2;
+			System.out.println(num);
+			i+=1;
+			num+=2;
 			
-//			int i = 100;
-//			while (i>= 0) {
-//				System.out.println (i);
-//				i-= 3;
+			int i = 100;
+			while (i>= 0) {
+				System.out.println (i);
+				i-= 3;
 		
-//			for (int i=1; i < 101; i++) {
-//			if (i % 2 == 1)
-//				System.out.println(i);
+			for (int i=1; i < 101; i++) {
+			if (i % 2 == 1)
+				System.out.println(i);
 		
 				
 		
