904c075333084e0db6294692b0fcc4c33d84ad8e
Desmond Young <60267316+DesmondYo@users.noreply.github.com>
2020-02-11 08:27:01 -0700

    Update Loops.java

--- JAVA Week 2/src/Loops.java
+++ JAVA Week 2/src/Loops.java
@@ -3,45 +3,42 @@ public class Loops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-				int i = 0;
-				int num = 0;
-				while (i<100) {
-					System.out.println(i);
-				
-					i = 100;
-					while (i >= 0) {
-						System.out.println (i);
-						i-= 3;	
-					}
-					
-					for (i=1; i < 100; i++) {
-						if (i % 2 == 1)
-						System.out.println(i);
-					}	
-					
-					for (i=0; i < 101; i++){
-					
-					
-						
-					if (i % 3 == 0 && i % 5 == 0) {
-				 			System.out.println("Hello World");
-				 		} else if (i % 3 == 0) {
-							System.out.println("Hello");
-						} else if (i % 5 == 0) {
-							System.out.println("World");
-						} else {
-							System.out.println(i);
-						}
+		int i = 0;
+		int num = 0;
+			while (i<100) {
+			System.out.println(i);
+			}
 			
-			
+			i = 100;
+			while (i >= 0) {
+				System.out.println (i);
+				i-= 3;	
+			}
+				for (i=1; i < 100; i++) {
+				if (i % 2 == 1)
+				System.out.println(i);
 			}	
-		
+			
+				for (i=0; i < 101; i++){
+			
+			
+				
+				if (i % 3 == 0 && i % 5 == 0) {
+		 			System.out.println("Hello World");
+		 		} else if (i % 3 == 0) {
+					System.out.println("Hello");
+				} else if (i % 5 == 0) {
+					System.out.println("World");
+				} else {
+					System.out.println(i);
+				}
+	
+	
+			}	
+
 		}	
 
 	}	
-
-}
 			
 			
 	
